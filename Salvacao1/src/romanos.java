51aff139a4f64fac8f5e222eee51207b6a519c03
35t3v40 <estevaoantuneslima@gmail.com>
2026-06-22 22:11:14 -0300

    proj salvacao1 add

---
+++ Salvacao1/src/romanos.java
@@ -0,0 +1,8 @@
+public class romanos {
+
+    public static void main (String[] args) {
+        System.out.println("A saber: Se com a tua boca confessares ao Senhor Jesus, e em teu coração creres que Deus o ressuscitou dentre os mortos, serás salvo.");
+        System.out.println("Romanos 10:9");
+    }
+
+}
